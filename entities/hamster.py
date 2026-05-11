1aa6f31b4ac405081b6c610586707f52478a5301
Lorena <lorenacp1@al.insper.edu.br>
2026-05-11 17:39:58 -0300

    arquivo hamster

---
+++ entities/hamster.py
@@ -0,0 +1,31 @@
+import pygame
+
+class Hamster:
+
+    def __init__(self, x, y):
+
+        self.x = x
+        self.y = y
+
+        self.radius = 20
+
+        self.vel_x = 0
+        self.vel_y = 0
+
+        self.gravity = 0.5
+
+    def update(self):
+
+        self.vel_y += self.gravity
+
+        self.y += self.vel_y
+        self.x += self.vel_x
+
+    def draw(self, screen):
+
+        pygame.draw.circle(
+            screen,
+            (160, 120, 80),
+            (int(self.x), int(self.y)),
+            self.radius
+        )

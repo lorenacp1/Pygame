c80fd4a6d9a6c7f453720689fe8918f40fe9003e
MarianaBatistussiGil <marianabg1@al.insper.edu.br>
2026-05-13 18:21:08 -0300

    para criar uma pagina inicial que muda o angulo para poder atirar

--- entities/hamster.py
+++ entities/hamster.py
@@ -8,50 +8,59 @@ class Hamster:
 
         self.rect = self.image.get_rect(topleft=(x, y))
 
-        self.rest_pos = (x, 600 - self.rect.height)
-
         self.vel_x = 0
         self.vel_y = 0
         self.gravity = 0.5
 
         self.dragging = False
         self.launched = False
-        self.intro_falling = True 
         self.ready = False
-
-    def update(self):
-        if self.dragging:
-            return
-        
-        if self.intro_falling:
-            self.vel_y += self.gravity
-            self.rect.y += int(self.vel_y)
-
-            if self.rect.bottom >= 600:
-                self.rect.bottom = 600
-                self.vel_y = 0
-                self.iintro_falling = False
-                self.ready = True
-
-            return
-        
-        if self.launched:
-            self.vel_y += self.gravity
-            self.rect.y += int(self.vel_y)
-            self.rect.x += int(self.vel_x)
-
-            if self.rect.bottom >= 600:
-                self.rect.bottom = 600
-                self.vel_y = 0
-                self.vel_x = 0
-                self.launched = Falseself.ready = True
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
-    def reset(self): 
-        self.rect.topleft = self.start_pos
+        self.intro_falling = True 
+    
+    def reset_to_intro(self, x, y):
+        self.rect.center = (x,y)
         self.vel_x = 0
         self.vel_y = 0
         self.dragging = False
         self.launched = False
+        self.ready = False
+        self.intro_falling = True
+
+    def prepare_for_game(self, x, y): 
+        self.rect.center = (x,y)
+        self.vel_x = 0
+        self.vel_y = 0
+        self.dragging = False
+        self.launched = False
+        self.ready = True
+        self.intro_falling = False
+
+    def update_intro(self, ground_y):
+        self.vel_y += self.gravity
+        self.rect.y += int(self.vel_y)
+
+        if self.rect.bottom >= ground_y:
+            self.rect.bottom = ground_yself.vel_y = 0
+            self.intro_falling = False
+            return True
+
+        return False
+
+    def update_game(self, ground_y): 
+        if self.dragging:
+            return 
+        
+        if self.launched: 
+            self.vel_x += int(self.vel_x)
+            self.vel_y += int(self.vel_y)
+            self.rect.y += int(self.vel_y)
+
+            if self.rect.bottom >= ground_y:
+                self.rect.bottom = ground_y
+                self.vel_x = 0
+                self.vel_y = 0
+                self.launched = False
+                self.ready = True
+                
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)

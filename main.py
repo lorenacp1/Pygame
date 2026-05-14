ad2f66d4191b718e691884ca769537385977c030
claradominguesplaza <clara.dp@al.insper.edu.br>
2026-05-13 22:57:11 -0300

    ajuste tamanho botão play

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def create_boxes():
 hamster = Hamster(*INTRO_SPAWN)
 boxes= create_boxes()
 gondola = pygame.Rect(830, 400, 290, 180)
-play_button = pygame.Rect(420, 410, 360, 75)
+play_button = pygame.Rect(420, 420, 360, 75)
 
 scene = "menu"
 running = True
